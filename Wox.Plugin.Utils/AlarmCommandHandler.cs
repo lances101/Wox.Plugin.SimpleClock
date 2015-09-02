c8b88e97fcd36728e8627670eeec17fa450842ad
Boris Makogonyuk <madrivus@gmail.com>
2015-09-02 21:22:10 +0100

    no message

---
+++ Wox.Plugin.Utils/AlarmCommandHandler.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wox.Plugin.Utils
+{
+    public class AlarmCommandHandler : CommandHandlerBase
+    {
+        public AlarmCommandHandler(PluginInitContext context): base(context)
+        {
+            
+        }
+
+        public override string CommandAlias
+        {
+            get{ return "alarm"; }
+        }
+
+        public override string CommandDescription
+        {
+            get
+            {
+                return "Allows to set an alarm";
+            }
+        }
+        public override List<Result> Query(Query query)
+        {
+            var args = query.ActionParameters;
+            List<Result> subCommands = new List<Result>();
+            subCommands.Add(new Result()
+            {
+                Title = "set",
+                SubTitle = "Set an alarm for a specific time",
+                Action = e =>
+                {
+                    if(args.Count > 2)
+                    {
+                        _context.API.ShowMsg("Settings alarm for " + args[2]);
+                        return false;
+                    }
+                    _context.API.ChangeQuery(String.Format("{0} alarm set ", _context.CurrentPluginMetadata.ActionKeyword), true);
+                    return false; 
+                }
+            }
+            );
+            subCommands.Add(new Result()
+            {
+                Title = "in",
+                SubTitle = "Set an alarm to fire after an amount of time",
+                Action = e =>
+                {
+                    _context.API.ChangeQuery(String.Format("{0} alarm set ", _context.CurrentPluginMetadata.ActionKeyword), true);
+                    return false;
+                }
+            }
+            );
+            if (args.Count > 1)
+            {
+                return subCommands.Where(r => query.ActionParameters[1].StartsWith(r.Title)).ToList();
+            }
+            return subCommands;
+        }
+
+        
+    }
+}
---
+++ Wox.Plugin.Utils/UtilsPlugin.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wox.Plugin.Utils
+{
+    public class Main : IPlugin
+    {
+        private PluginInitContext context;
+        private CommandHandlerBase[] _commandHandlers;
+        private List<Result> _results = new List<Result>();
+        public void Init(PluginInitContext context)
+        {
+            this.context = context;
+            _commandHandlers = new[]
+            {
+                new AlarmCommandHandler(context)
+            };
+
+            foreach (var handler in _commandHandlers)
+            {
+                _results.Add(new Result()
+                {
+                    Title = handler.CommandAlias,
+                    SubTitle = handler.CommandDescription,
+                    IcoPath = "Images\\utils.png",
+                    Action = e =>
+                    {
+                        context.API.ChangeQuery(String.Format("{0} {1} ", context.CurrentPluginMetadata.ActionKeyword, handler.CommandAlias ), true);
+                        return false;
+                    }
+                });
+            }
+        }
+        public List<Result> Query(Query query)
+        {
+            List<Result> results = new List<Result>();
+            
+            var args = query.ActionParameters;
+            var str = "";
+            for(int i = 0; i < args.Count; i++)
+            {
+                str += String.Format("{0}-{1}({2})|", i, args[i], args[i].Length);
+            }
+
+            if (args.Count == 0)
+            {
+                results = new List<Result>(_results);
+            }
+            else
+            {
+                var specificHandler = _commandHandlers.FirstOrDefault(r => r.CommandAlias == args[0].ToLower());
+                if (specificHandler != null)
+                {
+                    results.AddRange(specificHandler.Query(query));
+                }
+                else
+                {
+                    results = _results.Where(r => r.Title.ToLower().StartsWith(args[0].ToLower())).ToList();
+                }
+            }
+
+
+            results.Add(new Result()
+            {
+                Title = "Debugging",
+                SubTitle = "Args are " + str,
+                Action = e =>
+                {
+                    return false;
+                }
+            });
+            return results;
+        }
+        
+        
+    }
+}
